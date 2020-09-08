228a649092a6485b8a04c6671ae5a59f894d1f23
bappun <hello@baptiste-rouxel.com>
2020-09-08 21:10:47 +0200

    Purgecss added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const less = require('gulp-less');
 
 const htmlmin = require('gulp-htmlmin');
 const cssmin = require('gulp-clean-css');
+const purgecss = require('gulp-purgecss');
 const svgmin = require('gulp-svgmin');
 
 const destDir = './dist';
@@ -28,6 +29,9 @@ function compileCSS() {
     return src('./src/css/style.less')
         .pipe(less())
         .pipe(cssmin({ level: { 1: { specialComments: 0 } } }))
+        .pipe(purgecss({
+            content: ['./index.html'],
+        }))
         .pipe(dest(destDir));
 }
 
